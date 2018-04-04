e6f4041cfadefff76bc488afbc2b81eb8052d0ef
w_y_wei <1574057027@qq.com>
2018-04-04 20:33:46 +0800

    有道的书签翻译功能

--- 尝试0404.js
+++ 尝试0404.js
@@ -8,6 +8,8 @@
 // @grant        none   用于添加GM_ 函数到白名单
 // ==/UserScript==
 
-alert('hello,world')   // 编写错误了, 该先看看API接口的内容.先尝试调用接口
+// alert('hello,world')   // 编写错误了, 该先看看API接口的内容.先尝试调用接口
 
 // 解释内容的含义2018-4-4_19:36:27
+
+//   javascript:% 20void ((function ()% 20{ var% 20element % 20=% 20document.createElement('script'); element.id % 20=% 20'outfox_seed_js'; element.charset % 20=% 20'utf-8', element.setAttribute('src',% 20'http://fanyi.youdao.com/web2/seed.js?' % 20 +% 20Date.parse(new% 20Date())); document.body.appendChild(element); }) ())   看不懂有道的点击翻译功能
