1c4670aacda77e62a3b060b301a0b276d8944588
w_y_wei <1574057027@qq.com>
2018-04-04 19:36:47 +0800

    解释新建内容的含义

---
+++ 尝试0404.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
+
+// 解释内容的含义2018-4-4_19:36:27
