b0cca8c9c693e2fb80bcad3bac136f43263a602d
w_y_wei <1574057027@qq.com>
2018-04-10 13:48:10 +0800

    try1和try2以后用于对文件进行对比

--- try2.js
+++ try2.js
@@ -1,18 +1,1 @@
-'use strict';
-let http = require('http');
-
-
-let server = http.createServer(function (request, response) {
-
-
-    console.log(request.method + ': ' + request.url);
-
-    response.writeHead(200, {'Content-Type': 'text/html'});
-
-    response.end('<h1>Hello world!</h1>');
-});
-
-
-server.listen(8080);
-
-console.log('Server is running at http://127.0.0.1:8080/');
+// 用于对比
