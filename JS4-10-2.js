71b1c031d83ec96cf0be994af7fbbb9c8792ea5a
w_y_wei <1574057027@qq.com>
2018-04-10 13:16:35 +0800

    node使用fs模块读取文件

--- JS4-10-2.js
+++ JS4-10-2.js
@@ -1,4 +1,14 @@
-let greet = require('./JS4-10');
-let s = 'Michael';
+// let greet = require('./JS4-10');
+// let s = 'Michael';
 
-greet(s) // JS调用模块内容
+// greet(s) // JS调用模块内容
+'use strict';
+let fs = require('fs');
+
+fs.readFile('try.txt ','utf-8',function(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+});   //  node使用fs模块读取文件
