08a10a61aeb0ed4dfa4138e94bf2778b95e73999
w_y_wei <1574057027@qq.com>
2018-04-10 12:43:15 +0800

    制作模块

--- JS4-10.js
+++ JS4-10.js
@@ -2,3 +2,11 @@
 
 // console.log('hello, world');
 // console.log(100+200+3300);  //  试验vscode调用node
+
+let s = 'hello';
+
+function greet(name){
+    console.log(s + ',' + name + '!');
+}
+
+module.exports = greet;
