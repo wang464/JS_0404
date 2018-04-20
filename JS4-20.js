6a2a4df81faab1b3fd5fe4aea08662d72d7a84e7
w_y_wei <1574057027@qq.com>
2018-04-21 00:04:46 +0800

    内容尝试结束, https://zhuanlan.zhihu.com/p/24730075. 这个项目我看不懂了.需要停止

--- JS4-20.js
+++ JS4-20.js
@@ -1,4 +1,5 @@
 import { request } from 'http';
+import { Z_DEFLATED } from 'zlib';
 
 'use strict';
 // 获取图片
@@ -10,3 +11,22 @@ let queryHref = "http://www.haha.mx/topic/1/new/";   // 目标网址
 let querySearch = 1 ;   // 设置分页位置
 let urls = [] ;  // 所有需要下载的图片的地址
 
+function getHtml(href, search) {
+    let pageData = '';
+    let req  = http.get(href + search,function(res){
+        res.setEncoding('utf8');
+        res.on('data', function(chunk){
+            pageData += chunk; 
+        });
+        res.on('end', function(){
+            $ = cheerio.load(pageData);
+            let html = $('.joke-list-item .joke-main-content a img');
+
+            for(let i = 0 ; i < html.length ; i++){
+                let src =html[i].attribs.src;
+
+                if(src.indexOf(''))
+            }
+        })
+    }
+}
