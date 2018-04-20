43e7c4223b6f8c88a34a7e7d54e0bc79b6dab2a2
w_y_wei <1574057027@qq.com>
2018-04-21 00:33:50 +0800

    模块需要在使用的地方进行局部安装

--- JS4-20.js
+++ JS4-20.js
@@ -1,32 +1,31 @@
-import { request } from 'http';
-import { Z_DEFLATED } from 'zlib';
-
 'use strict';
 // 获取图片
-let http = require('http');   // http路由
-let cheerio = require('cheerio');  // html解析
-let fs = require('fs');   // 储存
+var cheerio = require('cheerio');
+var http = require('http');
+var iconv = require('iconv-lite');
 
-let queryHref = "http://www.haha.mx/topic/1/new/";   // 目标网址
-let querySearch = 1 ;   // 设置分页位置
-let urls = [] ;  // 所有需要下载的图片的地址
+var url = 'http://www.ygdy8.net/html/gndy/dyzz/index.html';
 
-function getHtml(href, search) {
-    let pageData = '';
-    let req  = http.get(href + search,function(res){
-        res.setEncoding('utf8');
-        res.on('data', function(chunk){
-            pageData += chunk; 
-        });
-        res.on('end', function(){
-            $ = cheerio.load(pageData);
-            let html = $('.joke-list-item .joke-main-content a img');
-
-            for(let i = 0 ; i < html.length ; i++){
-                let src =html[i].attribs.src;
-
-                if(src.indexOf(''))
-            }
+http.get(url, function(sres) {
+    var chunks = [];
+    sres.on('data', function(chunk) {
+      chunks.push(chunk);
+    });
+    // chunks里面存储着网页的 html 内容，将它zhuan ma传给 cheerio.load 之后
+    // 就可以得到一个实现了 jQuery 接口的变量，将它命名为 `$`
+    // 剩下就都是 jQuery 的内容了
+    sres.on('end', function() {
+      var titles = [];
+      //由于咱们发现此网页的编码格式为gb2312，所以需要对其进行转码，否则乱码
+      //依据：“<meta http-equiv="Content-Type" content="text/html; charset=gb2312">”
+      var html = iconv.decode(Buffer.concat(chunks), 'gb2312');
+      var $ = cheerio.load(html, {decodeEntities: false});
+      $('.co_content8 .ulink').each(function (idx, element) {
+        var $element = $(element);
+        titles.push({
+          title: $element.text()
         })
-    }
-}
+      })    
+      console.log(titles);     
+    });
+  });
