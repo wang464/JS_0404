51340aec8afdf104c34bf5d8118cf1391777dec4
w_y_wei <1574057027@qq.com>
2018-04-21 13:18:39 +0800

    构造函数

--- JS4-21.js
+++ JS4-21.js
@@ -1,10 +1,34 @@
+import { log } from "util";
+
 'use strict';
-function print(s) {
-    console.log(s);
-} // 函数命令   函数命令可以不使用;号结尾
+// function print(s) {
+//     console.log(s);
+// } // 函数命令   函数命令可以不使用;号结尾
 
 // var print = function(s){
 //     console.log(s);
 // }; // 函数表达式
 
-print('你好啊')
+// function foo(){
+//     return 'hello wold';
+// } // 构造函数
+
+// console.log(foo());  
+
+// var myFunc = function(){};
+
+// function test(f) {
+//     console.log(f.name);
+// };
+
+// test(myFunc); // 调用函数,显示打印结果
+
+function f(a,b) {
+    return a;
+}
+
+
+
+
+
+
