9c20ccb63c798e923c3ca372b167bfbf68cab53b
w_y_wei_job_mi <w_y_wei@qq.com>
2018-09-06 17:13:12 +0800

    没什么用的更改. 一个CSS样式居中

--- 0829-vue/src/App.vue
+++ 0829-vue/src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>  
+    <h1 v-text="title"></h1>
+    <input type="text" v-model="newItem" v-on:keyup.enter='addName' >
+    <!-- <HelloWorld/>   -->
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+// //引入hello组件现在暂时关闭
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+export default{
+  data:function () {
+    return{
+      title:'this is my todo list',
+      items:[
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+    toggleFinish:function (item) {
+      item.isFinished = ! item.isFinished
+    },
+    addNew:function () {
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem=''
+    }
   }
 }
+
+
 </script>
 
 <style>
