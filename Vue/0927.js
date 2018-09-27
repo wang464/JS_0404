edf0cb4df52c5347b8849324cdb76d9d2272411a
w_y_wei_job_mi <w_y_wei@qq.com>
2018-09-27 16:44:03 +0800

    不使用组件实现居中

---
+++ Vue/0927.js
@@ -0,0 +1,13 @@
+'use strict';
+// var value = ''
+let lis_t = ["q", "w", "e", "r", "t"];
+
+for (let [key, value] of lis_t) {
+    console.log(key);
+}
+
+// let iterable = [10, 20, 30];
+
+// for (const value of iterable) {
+//   console.log(value);
+// }
