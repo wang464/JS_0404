4d03f5c358b40ea05ccab2882cee1ec6052faeae
w_y_wei_job_mi <w_y_wei@qq.com>
2018-09-27 21:40:54 +0800

    abba

--- Vue/0927.js
+++ Vue/0927.js
@@ -82,10 +82,10 @@ while (i <= 1000) {
   } else if (x == 2 && num == 2) {
     x = 3;
     // console.log( "第二个");
-  } else if (x == 3 && num == 1) {
+  } else if (x == 3 && num == 2) {
     x = 4;
     // console.log( "第三个")
-  } else if (x == 4 && num == 2) {
+  } else if (x == 4 && num == 1) {
     x = 1;
     // console.log( "第四个");
     mub ++;
