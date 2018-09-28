ef5804be7aa7572b1e0439a2433f3e8d63d4b686
w_y_wei_job_mi <w_y_wei@qq.com>
2018-09-29 05:18:25 +0800

    一只小企鹅

---
+++ Vue/0928.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+10000从20开始翻倍. 可以走几步.
+使用for循环就可以.
+设置一个数是x =10000
+当y 小于 x时运行
+z = 20
+z =  z *2
+
+
+
+
+*/
+var yy = function (y) {
+
+  for (var i = 1; i <= 9; i++) {
+
+    y.push(i);
+    // console.log(i);
+    console.log(y.length);
+  }
+}
+
