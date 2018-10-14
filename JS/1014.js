ae3abf9326fcad68f2fed5ed7caad2d8edcf9e8b
w_y_wei_job_mi <w_y_wei@qq.com>
2018-10-15 00:20:07 +0800

    构造函数和对象

--- JS/1014.js
+++ JS/1014.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// 写一个对象
+
+let dock = {
+  name: 'After',
+  number: 2,
+  say: function () {
+    return console.log('你好啊');
+  }
+}
+console.log(dock.say);
+
+// 构造函数需要用空格分开一下?
+// 为什么我写的不符合严格模式? 严格模式的构造函数怎么写?  还是因为我自己写的规则问题?
+
 function Dogs () {
   this.name = 'nih';
   this.year = '13';
@@ -7,5 +21,3 @@ function Dogs () {
 
 let myDogs = new Dogs();
 console.log(myDogs.name);
-// 构造函数需要用空格分开一下?
-// 为什么我写的不符合严格模式? 严格模式的构造函数怎么写?  还是因为我自己写的规则问题?
