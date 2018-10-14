f547e2a57a70d416ca71962ae26304a9a299ca7f
w_y_wei_job_mi <w_y_wei@qq.com>
2018-10-15 00:08:31 +0800

    使用了eslint的规则

---
+++ JS/1014.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function Dogs () {
+  this.name = 'nih';
+  this.year = '13';
+}
+
+let myDogs = new Dogs();
+console.log(myDogs.name);
+// 构造函数需要用空格分开一下?
+// 为什么我写的不符合严格模式? 严格模式的构造函数怎么写?  还是因为我自己写的规则问题?
